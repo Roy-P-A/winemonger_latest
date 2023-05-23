02c6bf776e993b0d25fce7e925bd4a0f88a96514
Apple <apple@Apples-MacBook-Pro.local>
2023-05-23 21:05:51 +0530

    second commit

---
+++ lib/features/widgets/appbar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:iconify_flutter/icons/uiw.dart';
+import 'package:winemonger/utils/theme/theme.dart';
+// import 'package:badges/badges.dart';
+
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  const CustomAppBar({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.appbarbackgroundColor,
+      title: Text(
+        title,
+        style: TextStyle(
+          color: Theme.of(context).colorScheme.appbarTextColor,
+          fontWeight: FontWeight.w600,
+          fontSize: 17,
+        ),
+      ),
+      centerTitle: true,
+      leading: Builder(builder: (context) {
+        return IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Theme.of(context).colorScheme.appbarTextColor,
+          ),
+          onPressed: () => Scaffold.of(context).openDrawer(),
+        );
+      }),
+      actions: [
+        Badge(
+          //position: BadgePosition.topEnd(top: 14, end: 12),
+          child: IconButton(
+            icon: Iconify(
+              Uiw.bell,
+              color: Theme.of(context).colorScheme.appbarTextColor,
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ],
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(3.0),
+        child: Container(
+          color: Theme.of(context).colorScheme.appbarBorderColor,
+          height: 3.0,
+        ),
+      ),
+      elevation: 0,
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(60);
+}
---
+++ lib/utils/routes.dart
@@ -0,0 +1,56 @@
+import 'package:get/route_manager.dart';
+import '../features/dashboard/dashboard_screen.dart';
+import '../features/login/login_screen.dart';
+
+List<GetPage> routes() => [
+      GetPage(
+          name: "/", page: () => Dashboard(), transition: Transition.cupertino),
+      GetPage(
+          name: "/dashboard",
+          page: () => const Dashboard(),
+          transition: Transition.cupertino),
+      GetPage(
+          name: "/login",
+          page: () => const LogIn(),
+          transition: Transition.cupertino),
+      GetPage(
+          name: "/orders",
+          page: () => const Dashboard(),
+          transition: Transition.cupertino),
+      GetPage(
+          name: "/orderdetailscreen",
+          page: () => const Dashboard(),
+          transition: Transition.cupertino),
+      GetPage(
+          name: "/tastings",
+          page: () => const Dashboard(),
+          transition: Transition.cupertino),
+      GetPage(
+          name: "/reservations",
+          page: () => const Dashboard(),
+          transition: Transition.cupertino),
+      GetPage(
+          name: "/manufacturers",
+          page: () => const Dashboard(),
+          transition: Transition.cupertino),
+      GetPage(
+          name: "/sales",
+          page: () => const Dashboard(),
+          transition: Transition.cupertino),
+      GetPage(
+          name: "/products",
+          page: () => const Dashboard(),
+          transition: Transition.cupertino),
+      GetPage(
+          name: "/customers",
+          page: () => const Dashboard(),
+          transition: Transition.cupertino),
+      GetPage(
+          name: "/imports",
+          page: () => const Dashboard(),
+          transition: Transition.cupertino),
+      GetPage(
+          name: "/createorders",
+          page: () => const Dashboard(),
+          transition: Transition.cupertino),
+    ];
